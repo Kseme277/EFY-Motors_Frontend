3c3bb9b4cc0f57142f1df9f093f1467c3671c3ed
kseme277 <kseme277@gmail.com>
2026-05-03 23:19:22 +0100

    Correctifs Mineur

--- src/app/features/cars/cars.component.ts
+++ src/app/features/cars/cars.component.ts
@@ -5,7 +5,6 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, ActivatedRoute } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ApiService } from '../../services/api.service';
-import { ToastrService } from 'ngx-toastr';
 import { SweetAlertService } from '../../services/sweet-alert.service';
 
 interface Car {
@@ -112,7 +111,6 @@ export class CarsComponent implements OnInit, OnDestroy {
   constructor(
     private apiService: ApiService,
     private route: ActivatedRoute,
-    private toastr: ToastrService,
     private swal: SweetAlertService
   ) {}
 
@@ -572,15 +570,8 @@ export class CarsComponent implements OnInit, OnDestroy {
     event.preventDefault();
     const url = window.location.origin + '/cars/' + carId;
     navigator.clipboard.writeText(url).then(() => {
-      // On utilise SweetAlert pour plus de fiabilité si toastr ne s'affiche pas
+      // On utilise SweetAlert pour plus de fiabilité
       this.swal.toast('Lien du véhicule copié !', 'success');
-      
-      // On garde aussi toastr au cas où
-      this.toastr.success('Le lien du véhicule a été copié !', 'Succès', {
-        progressBar: true,
-        closeButton: true,
-        positionClass: 'toast-bottom-right'
-      });
     });
   }
 }
